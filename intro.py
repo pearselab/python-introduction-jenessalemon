eefb541f6075d24f465f06935e4be00259a59655
Your Name <jimblotter@Jenessa.local>
2016-11-16 11:10:18 -0700

    First commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+#1
+for i in range(20, 9, -1):
+    print(i)
+
+#2
+a = [i for i in range(20, 9, -1)]
+print(a)
+
+#3
+#have to work on plants while our conversation is fresh on my mind!
