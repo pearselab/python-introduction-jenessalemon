02e4bdf59f998364706f488ce00a92d5123b2eac
Your Name <jimblotter@Jenessa.local>
2016-11-18 21:45:06 -0700

    Friday night

--- intro.py
+++ intro.py
@@ -1,5 +1,3 @@
-print("Hello, World!")
-
 #1
 for i in range(20, 9, -1):
     print(i)
@@ -7,6 +5,34 @@ for i in range(20, 9, -1):
 #2
 a = [i for i in range(20, 9, -1)]
 print(a)
+print("Hello")
 
 #3
-#have to work on plants while our conversation is fresh on my mind!
+for i in range(20, 9, -1):
+    if i % 2 == 0:
+        print(i)
+print("World")
+
+#4
+b = [i for i in range(20, 9, -1) if i % 2 == 0]
+print(b)
+
+#5
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):          #only need to test from 3 to the square root of n
+        if n % i == 0:
+            return False
+    return True
+
+print is_prime(2) #True
+print is_prime(4) #False
+print is_prime(7) #True
+print is_prime(13) #True
+
+#6
